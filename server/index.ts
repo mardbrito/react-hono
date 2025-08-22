98b6887971ebb4378c8e1a80b5266ccd5581dd57
mardbrito <marcelo4web@gmail.com>
2025-08-22 18:58:51 -0400

    feat: add production build script

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,18 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
-const route = app.get("/api/books", (c) => {
-  return c.json([
-    { id: 1, title: "1984", author: "George Orwell" },
-    { id: 2, title: "To kill a Mockingbirb", author: "Harper Lee" },
-    { id: 3, title: "The great Gatsby", author: "F. Scoot Fitzgerald" },
-    { id: 4, title: "The catcher in the Rye", author: "J.D. Salinger" },
-  ]);
-});
+const route = app
+  .use("*", serveStatic({ path: "./client/dist" }))
+  .get("/api/books", (c) => {
+    return c.json([
+      { id: 1, title: "1984", author: "George Orwell" },
+      { id: 2, title: "To kill a Mockingbirb", author: "Harper Lee" },
+      { id: 3, title: "The great Gatsby", author: "F. Scoot Fitzgerald" },
+      { id: 4, title: "The catcher in the Rye", author: "J.D. Salinger" },
+    ]);
+  });
 
 export type AppType = typeof route;
 
