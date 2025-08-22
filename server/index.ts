278f2fcdb988c1f2ffc0545d1a6d15dd7e474e02
mardbrito <marcelo4web@gmail.com>
2025-08-22 18:15:55 -0400

    first commit

---
+++ server/index.ts
@@ -0,0 +1,14 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/api/books", (c) => {
+  return c.json([
+    { id: 1, title: "1984", author: "George Orwell" },
+    { id: 2, title: "To kill a Mockingbirb", author: "Harper Lee" },
+    { id: 3, title: "The great Gatsby", author: "F. Scoot Fitzgerald" },
+    { id: 4, title: "The catcher in the Rye", author: "J.D. Salinger" },
+  ]);
+});
+
+export default app;
