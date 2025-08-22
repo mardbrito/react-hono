059677f02e5fd51a527b9652c7a1f3346bf5e852
mardbrito <marcelo4web@gmail.com>
2025-08-22 18:47:28 -0400

    feat: config data fetching

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import { type AppType } from "../../server/index";
+import { hc, type InferResponseType } from "hono/client";
+
+const client = hc<AppType>("/");
+
 function App() {
-  return <div>Hello</div>;
+  const [books, setBooks] = useState<
+    InferResponseType<typeof client.api.books.$get>
+  >([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const resp = await client.api.books.$get();
+      const data = await resp.json();
+      setBooks(data);
+    };
+    fetchBooks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Book list</h1>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            {book.title} {book.author}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/api/books", (c) => {
+const route = app.get("/api/books", (c) => {
   return c.json([
     { id: 1, title: "1984", author: "George Orwell" },
     { id: 2, title: "To kill a Mockingbirb", author: "Harper Lee" },
@@ -10,5 +10,7 @@ app.get("/api/books", (c) => {
     { id: 4, title: "The catcher in the Rye", author: "J.D. Salinger" },
   ]);
 });
+
+export type AppType = typeof route;
 
 export default app;
